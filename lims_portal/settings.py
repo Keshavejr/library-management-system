b77ca7f11a85cb6e69d8838074f34c31d505f31c
keshave basnet <keshavecr7@gmail.com>
2026-04-27 22:27:41 +0530

    use env secret key

--- lims_portal/settings.py
+++ lims_portal/settings.py
@@ -19,7 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hje3)9fmmrt%ljwt48s%r*a5-f!p5fbnvfq$sr1(w&^q+-s&7w'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
